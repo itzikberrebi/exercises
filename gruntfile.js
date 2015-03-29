a7135cecacc2c267d14eb76ead30bbbbdbe00da2
itzikberrebi <itzikb800@gmail.com>
2015-03-29 17:23:29 +0300

    grunt +

---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+
+    md2html: {
+      one_file: {
+        options: {},
+        files: [{
+          src: ['index.md'],
+          dest: 'outline.html'
+        }]
+      }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-md2html');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
